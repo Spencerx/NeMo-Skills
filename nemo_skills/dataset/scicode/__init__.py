f53fb0b9d84a09411b0d13c21ea08a3ae9141d2a
Matvei Novikov <mnovikov@nvidia.com>
2026-05-29 10:38:13 -0700

    updated scicode to match aa methodology (#1469)
    Signed-off-by: mnovikov <mnovikov@nvidia.com>

--- nemo_skills/dataset/scicode/__init__.py
+++ nemo_skills/dataset/scicode/__init__.py
@@ -14,7 +14,9 @@
 
 # settings that define how evaluation should be done by default (all can be changed from cmdline)
 METRICS_TYPE = "scicode"
-GENERATION_ARGS = "++prompt_config=eval/scicode/default ++eval_type=scicode"
+# Use the multistep ("scientist-annotated background is provided, write code directly")
+# prompt to match the prompt Artificial Analysis actually sends to the endpoint.
+GENERATION_ARGS = "++prompt_config=eval/scicode/background ++eval_type=scicode"
 GENERATION_MODULE = "nemo_skills.inference.eval.scicode"
 REQUIRES_SANDBOX = True
-EVAL_SPLIT = "test_aai"  # default to test + validation for consistency with AAI
+EVAL_SPLIT = "test"  # 65 problems / 288 subproblems, matching AA's SciCode methodology
